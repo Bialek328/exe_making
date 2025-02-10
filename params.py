beabbc1a890281fa881d1be3a3b4a4e4337c14b4
Mateusz <bialek328@gmail.com>
2025-02-10 18:45:50 +0100

    adjust params so it reads external file

--- params.py
+++ params.py
@@ -1,11 +1,13 @@
 import configparser
+import os 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(BASE_DIR, "config.ini")
 
 config = configparser.ConfigParser()
-try:
-    config.read("config.ini")
-
-except FileNotFoundError as e:
-    print("no config file found")
-
+if os.path.exists(CONFIG_PATH):
+    config.read(CONFIG_PATH)
+else:
+    raise FileNotFoundError("Configuration file not found")
 
 PARAM1 = config.get("DEFAULT", "PARAM1")
